2bbfd2514b576f796701bcbcee27b4cc53494c15
errski <69652270+errski@users.noreply.github.com>
2021-02-03 11:09:00 -0500

    added javascript
    Added javascript animation to see changes to home page. Allowed the poll to do something using Javascript.

---
+++ index.js
@@ -0,0 +1,47 @@
+/*document.addEventListener("keydown",function(event) {
+  console.log(event);
+  setTimeout(textAnimation(),1000)});
+*/
+
+
+textAnimation();
+
+pollResults = document.querySelector(".poll-results");
+//alert(pollResults.innerHTML);
+
+pollForm = document.forms["pollform"];
+var radios = pollForm.elements["myradio"];
+pollForm.elements["send"].onclick = function() {
+  //alert("clicked");
+  for(var j = 0; j<radios.length; j++) {
+    if(radios[j].checked) {
+       pollResults.querySelector("p").innerText = "You selected " + radios[j].value;
+       pollResults.classList.remove("invisible");
+    }
+  }
+}
+
+  for(var i = 0; i< radios.length; i++) {
+    radios[i].onclick = function() {
+        //alert(this.value);
+        for(var j = 0; j<radios.length; j++) {
+          if(this.value != radios[j].value) {
+            radios[j].checked = false;
+          }
+        }
+    }
+  }
+
+function removeInvisible(item) {
+  item.classList.remove("invisible");
+}
+function textAnimation() {
+
+    setTimeout(function() {
+      document.querySelector("h1").innerHTML = "Love To Read";
+    }, 3000);
+    setTimeout(function() {
+      document.querySelector("h1").innerHTML = "We Read Books";
+    }, 5000);
+    setTimeout(textAnimation,5000);
+}
